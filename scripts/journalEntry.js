48568b2f8fcf96d62b28758db47b36de496cd2c1
rickiele <rickienthefishies@gmail.com>
2020-11-19 15:42:06 -0600

    added edit and delete buttons

--- scripts/journalEntry.js
+++ scripts/journalEntry.js
@@ -5,9 +5,15 @@
 export const JournalEntryComponent = (entry) => {
     return `
         <section id="entry--${entry.id}" class="journalEntry">
-        <div class="entry__journalDate"> ${entry.date}</div> 
-        <div class="entry__journalConcept"> ${entry.concept}</div> 
-        <div class="entry__journalText"> ${entry.entry}</div>   
+
+            <div class="entry__journalDate"> ${entry.date}</div> 
+            <div class="entry__journalConcept"> ${entry.concept}</div> 
+            <div class="entry__journalText"> ${entry.entry}</div>   
+
+            <div class="entry__buttonDiv">
+                <button class="entry__editButton">Edit</button>
+                <button class="entry__deleteButton">Delete</button>
+             </div>
         </section>
     `
 }
